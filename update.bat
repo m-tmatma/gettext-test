dafc6dc0d787df992132c2447288db0bf8db632c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-25 09:58:21 +0900

    同一ファイル名で変換する

--- update.bat
+++ update.bat
@@ -3,6 +3,6 @@ cd /d %~dp0
 setlocal
 
 set PATH=%PATH%;C:\Program Files\Git\usr\bin
-msgmerge.exe  locale\ja_JP\LC_MESSAGES\messages.po locale\messages.pot -o locale\ja_JP\LC_MESSAGES\messages-updated.po
+msgmerge.exe  locale\ja_JP\LC_MESSAGES\messages.po locale\messages.pot -o locale\ja_JP\LC_MESSAGES\messages.po
 
 endlocal
